b4878cb7ad4be315bee94512403b6a2658809fd8
Arsha Anvekar <arshaanvekar4@gmail.com>
2026-03-29 16:49:12 -0700

    changed the cors so that adding applications can work

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000' }));
+app.use(cors({ 
+  origin: [
+    process.env.CLIENT_URL || 'http://localhost:3000',
+    'https://application-tracker-hthx.vercel.app',
+    'http://localhost:3000'
+  ]
+}));
 app.use(express.json());
 
 app.use('/api', routes);
