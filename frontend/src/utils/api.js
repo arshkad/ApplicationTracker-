24029eaedae04769b446cec44fe2fc939f0c962a
Arsha Anvekar <arshaanvekar4@gmail.com>
2026-03-29 13:31:36 -0700

    changed the const api in api.js to fix the display of adding an application

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ 
+    baseURL: (process.env.REACT_APP_API_URL || '') + '/api'
+  });
 
 // Applications
 export const getApplications = (params) => api.get('/applications', { params });
